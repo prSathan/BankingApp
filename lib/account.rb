faba56550786d985e657b674e9785646ff0de615
Prabu Sathan <prabu.sathan@gmail.com>
2017-07-11 11:24:35 +0100

    Test to check that an amount can be deposited into the account and updates balance. Created a deposit method that takes an amount and adds it to the balance

--- lib/account.rb
+++ lib/account.rb
@@ -5,5 +5,9 @@ attr_reader :balance
   def initialize
     @balance = 0
   end
-  
+
+  def deposit(amount)
+    @balance += amount
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,4 +6,9 @@ describe Account do
   it 'can create an account that initializes with a balance of zero' do
     expect(account.balance).to equal(0)
   end
+
+  it 'can deposit money into an account' do
+    account.deposit(1000)
+    expect(account.balance).to equal(1000)
+  end
 end
